cc80729df1c96d007b5d25ee631c39a9dc9c6e64
JunyiZhou <zhoujunyi_hit@163.com>
2016-01-05 15:03:34 +0800

    add grid layout with span size change

--- RecyclerViewDemo/app/src/main/java/com/example/junyizhou/recyclerviewdemo/activity/MainActivity.java
+++ RecyclerViewDemo/app/src/main/java/com/example/junyizhou/recyclerviewdemo/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 
 import com.example.junyizhou.recyclerviewdemo.R;
 import com.example.junyizhou.recyclerviewdemo.fragment.GridListDemoFragment;
+import com.example.junyizhou.recyclerviewdemo.fragment.GridSpanSizeDIYListDemoFragment;
 import com.example.junyizhou.recyclerviewdemo.fragment.LinearListDemoFragment;
 import com.example.junyizhou.recyclerviewdemo.fragment.StaggeredGridHorizontalListDemoFragment;
 import com.example.junyizhou.recyclerviewdemo.fragment.StaggeredGridVerticalFlowListDemoFragment;
@@ -19,19 +20,12 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         viewInit();
-//        mRecyclerView = (RecyclerView) findViewById(R.id.id_recyclerview);
-//        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 4));
-//        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL));
-//        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.HORIZONTAL));
-//        mRecyclerView.setAdapter(mAdapter = new HomeAdapter());
-//        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
-//        mRecyclerView.addItemDecoration(new DividerGridItemDecoration(this));
     }
 
     public void viewInit() {
         findViewById(R.id.btn_linear).setOnClickListener(this);
         findViewById(R.id.btn_grid).setOnClickListener(this);
+        findViewById(R.id.btn_grid_span_size_diy).setOnClickListener(this);
         findViewById(R.id.btn_staggered_grid_vertical).setOnClickListener(this);
         findViewById(R.id.btn_staggered_grid_horizontal).setOnClickListener(this);
         findViewById(R.id.btn_staggered_grid_vertical_flow).setOnClickListener(this);
@@ -46,6 +40,9 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
                 break;
             case R.id.btn_grid:
                 getSupportFragmentManager().beginTransaction().replace(R.id.container, new GridListDemoFragment()).addToBackStack(null).commit();
+                break;
+            case R.id.btn_grid_span_size_diy:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, new GridSpanSizeDIYListDemoFragment()).addToBackStack(null).commit();
                 break;
             case R.id.btn_staggered_grid_vertical:
                 getSupportFragmentManager().beginTransaction().replace(R.id.container, new StaggeredGridVerticalListDemoFragment()).addToBackStack(null).commit();
---
+++ RecyclerViewDemo/app/src/main/java/com/example/junyizhou/recyclerviewdemo/fragment/GridSpanSizeDIYListDemoFragment.java
@@ -0,0 +1,84 @@
+package com.example.junyizhou.recyclerviewdemo.fragment;
+
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.junyizhou.recyclerviewdemo.R;
+import com.example.junyizhou.recyclerviewdemo.decoration.DividerGridItemDecoration;
+import com.example.junyizhou.recyclerviewdemo.foundation.RecyclerListFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by JunyiZhou on 2016/1/4.
+ */
+public class GridSpanSizeDIYListDemoFragment extends RecyclerListFragment {
+
+    private List<String> mDataList;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initData();
+    }
+
+    @Override
+    public ViewHolder getViewHolder(ViewGroup parent) {
+        return new CharViewHolder(parent);
+    }
+
+    protected void initData() {
+        mDataList = new ArrayList<>();
+        for (int i = 'A'; i < 'z'; i++) {
+            mDataList.add("" + (char) i);
+        }
+        setDataList(mDataList);
+    }
+
+    @Override
+    public RecyclerView.LayoutManager createLayoutManager() {
+        GridLayoutManager manager = new GridLayoutManager(getActivity(), 3);
+        manager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                return (3 - position % 3);
+            }
+        });
+        return manager;
+    }
+
+    @Override
+    public RecyclerView.ItemDecoration createItemDecoration() {
+        return new DividerGridItemDecoration(getActivity());
+    }
+
+    @Override
+    public RecyclerView.ItemAnimator createItemAnimator() {
+        return null;
+    }
+
+    class CharViewHolder extends ViewHolder<String> {
+
+        TextView tvChar;
+
+        public CharViewHolder(ViewGroup parent) {
+            this(LayoutInflater.from(getActivity()).inflate(R.layout.item_grid, parent, false));
+        }
+
+        public CharViewHolder(View view) {
+            super(view);
+            tvChar = (TextView) view.findViewById(R.id.tv_char);
+        }
+
+        @Override
+        public void bind(String item, int position) {
+            tvChar.setText(item);
+        }
+    }
+}
